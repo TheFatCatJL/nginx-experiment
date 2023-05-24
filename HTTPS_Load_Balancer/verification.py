a57ffb05b1e866002ce057d69251d19752e9c9a9
TheFatCatJL <36097649+TheFatCatJL@users.noreply.github.com>
2023-05-24 17:00:09 +0800

    Update verification.py

--- HTTPS_Load_Balancer/verification.py
+++ HTTPS_Load_Balancer/verification.py
@@ -4,7 +4,7 @@ import requests,os
 # Here flask will get the CA information from ca-ssl.pem
 # What is this like in the real world context? In the real world, we will be like our own closed up env where our servers
 # recognise us as the CA, but any one outside when attempting to use their own CA file (which is publicly certified - and chained) in their OS
-# Will come to our server and the CA file they have WILL NOT recognise us because we are not a public CA.
+# Will come to our server and the CA file they have WILL NOT recognise us because we are not a public CA, thus our public cert will deemed UNSAFE
 
 # So here we point to the CA cile we created in README- step 2
 os.environ["REQUESTS_CA_BUNDLE"] = "certs/ca-ssl.pem"
